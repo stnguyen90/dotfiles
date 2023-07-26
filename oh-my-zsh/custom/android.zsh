de2bdf96af28a563627952e42affbf746ca38152
Steven Nguyen <stnguyen90@users.noreply.github.com>
2023-07-26 13:59:32 -0700

    Set ANDROID_SDK_ROOT

--- oh-my-zsh/custom/android.zsh
+++ oh-my-zsh/custom/android.zsh
@@ -1,3 +1,4 @@
 if [ -d $HOME/Library/Android/sdk/platform-tools ]; then;
     export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools;
+    export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
 fi;
